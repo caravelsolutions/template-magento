44568d558cb1a79d0507229588cd93733182305e
Heitor Ramon Ribeiro <heitor.ramon@gmail.com>
2021-08-03 12:56:07 -0300

    chore: fixed plugin import

--- plugins/bonaventure.js
+++ plugins/bonaventure.js
@@ -2,6 +2,6 @@ import Vue from 'vue'
 import { install } from '@caravelx/bonaventure';
 
 import '@caravelx/bonaventure/src/assets/font/bonaventure-icon-line/bonaventure-icon-line.scss';
-import '@caravelxs/bonaventure/src/assets/font/bonaventure-icon-fill/bonaventure-icon-fill.scss';
+import '@caravelx/bonaventure/src/assets/font/bonaventure-icon-fill/bonaventure-icon-fill.scss';
 
 Vue.use(install, { all: true });
